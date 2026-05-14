9d44d14388c59f6faa78075878a0bff21993a475
MatheusRw <mt.richaard@hotmail.com>
2026-05-14 12:46:34 -0300

    Criando funcao geradora

---
+++ web/app/_components/diet-generator.tsx
@@ -0,0 +1,7 @@
+export function DietGenerator() {
+  return (
+    <>  
+    <h1>Teste</h1>
+    </>
+  )
+}
--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,9 +1,33 @@
 "use client";
 
 import { DietForm } from "./_components/diet-form";
+import { useState } from "react";
+import { DietGenerator } from "./_components/diet-generator";
+
+interface DietData {
+  nome: string;
+  idade: number;
+  altura_cm: number;
+  peso_kg: number;
+  sexo: "masculino" | "feminino";
+  nivel_atividade: "sedentario" | "2x_semana" | "4x_semana";
+  objetivo: "perda_de_peso" | "hipertrofia" | "manter_massa_muscular";
+}
 
 export default function Home() {
+  const [data, setData] = useState<DietData | null>(null);
+
+  function handleSubmit(userInfo: DietData) {
+    setData(userInfo);
+  }
+
   return (
-    <DietForm onSubmit={() => {}} />
+    <>
+      {!data ? (
+        <DietForm onSubmit={handleSubmit} />
+      ) : (
+        <DietGenerator data={data} />
+      )}
+    </>
   );
-}
+} // ✅ fechamento da função Home
