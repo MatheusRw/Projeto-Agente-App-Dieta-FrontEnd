b2c59861cef6a2961ef9b3d0d05bad1060601166
MatheusRw <mt.richaard@hotmail.com>
2026-05-14 21:10:33 -0300

    Acrescentando o formulario e varias coisas

--- web/app/_components/diet-generator.tsx
+++ web/app/_components/diet-generator.tsx
@@ -1,7 +1,105 @@
-export function DietGenerator() {
-  return (
-    <>  
-    <h1>Teste</h1>
-    </>
-  )
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { Loader, Sparkles } from "lucide-react";
+import { DietData } from "../types/diet-data";
+import { useRef, useState } from "react";
+
+
+
+
+export function DietGenerator({data }: {data: DietData}) {
+
+    const [output,setOutput] = useState("")
+    const [isStreaming,setIsStreaming] = useState(false)
+
+    const controllerRef = useRef<AbortController | null>(null)
+
+    async function startStreaming() {
+        const controller = new AbortController()
+        controllerRef.current = controller
+
+        setOutput("")
+        setIsStreaming(true)
+
+        try{
+
+            const response = await fetch("http://localhost:3333/plan", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    nome: data.nome,
+                    idade: data.idade,
+                    altura_cm: data.altura_cm,
+                    peso_kg: data.peso_kg,
+                    sexo: data.sexo,
+                    nivel_atividade: data.nivel_atividade,
+                    objetivo: data.objetivo,
+                    }),
+                    signal: controller.signal,
+            })
+
+
+            const reader = response.body?.getReader()
+            const decoder = new TextDecoder("utf-8")
+
+            while(true) {
+                const { done, value } = await reader!.read()
+                if(done) break;
+                
+                setOutput((prev) => prev + decoder.decode(value))
+            }
+            
+
+        }catch(err: any){
+            if(err.name === "AbortError") {
+                console.log("Stream aborted");
+                return;
+            }
+            console.log("Error fetching data:", err)
+
+        }finally{
+            setIsStreaming(false);
+            controllerRef.current = null
+
+        }
+        
+    }
+
+    async function handlegenerate() {
+        if(isStreaming) {
+            controllerRef.current?.abort()
+            setIsStreaming(false)
+            return
+        }
+        await startStreaming();
+
+    }
+
+
+    return (
+        <div className="min-h-screen flex flex-col items-center justify-center p-4">
+        <Card className="w-full max-w-4xl border-0 p-4 md:p-6">
+
+            <div className="flex justify-center gap-4">
+            <Button
+                className="cursor-pointer gap-2"
+                size="lg"
+                onClick={handlegenerate}>
+                <Sparkles className="w-5 h-5" />
+                Gerar dieta
+            </Button>
+            </div>
+            <div className="bg-card rounded-lg p-6 border border-border max-h[500px] overflow-y-auto ">
+                <div className="prose prose-sm max-w-none">
+                    {output}
+                </div>
+            </div>
+        </Card>
+        </div>
+    )
+
 }
--- web/app/page.tsx
+++ web/app/page.tsx
@@ -3,16 +3,9 @@
 import { DietForm } from "./_components/diet-form";
 import { useState } from "react";
 import { DietGenerator } from "./_components/diet-generator";
+import { DietData } from "./types/diet-data";
 
-interface DietData {
-  nome: string;
-  idade: number;
-  altura_cm: number;
-  peso_kg: number;
-  sexo: "masculino" | "feminino";
-  nivel_atividade: "sedentario" | "2x_semana" | "4x_semana";
-  objetivo: "perda_de_peso" | "hipertrofia" | "manter_massa_muscular";
-}
+
 
 export default function Home() {
   const [data, setData] = useState<DietData | null>(null);
